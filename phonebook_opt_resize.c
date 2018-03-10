ef86bcda6de344445fcf1543de2587a7a8bd7646
hungyuhang <f74036093@mail.ncku.edu.tw>
2018-03-10 21:41:34 +0800

    Add phonebook_opt_resize.c and .h

---
+++ phonebook_opt_resize.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "phonebook_opt.h"
+
+/* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
+entry *findName(char lastName[], entry *pHead)
+{
+    /* TODO: implement */
+    return NULL;
+}
+
+entry *append(char lastName[], entry *e)
+{
+    /* TODO: implement */
+    return NULL;
+}
